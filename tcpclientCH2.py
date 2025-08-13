82349f50c3aa3520844b18de0ab565a094b77b7d
Goose <cybergoose13@gmail.com>
2025-08-12 18:43:09 -0700

    tcp client simple connection, minor changes to original code from book

--- tcpclientCH2.py
+++ tcpclientCH2.py
@@ -1,6 +1,21 @@
 import socket
 
-target_host = "wwww.google.com"
-target_port = "80"
+
+# Code from book was not working with target_host set as a string
+# I had to get the IP address by using socket.gethostbyname
+# this way IP was returned and passed into client.connect
+# target_host = 'www.google.com'
+target_host = socket.gethostbyname('www.google.com')
+target_port = 80
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+client.connect((target_host, target_port))
+
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+response = client.recv(4096)
+
+print(f'{response.decode()}')
+
+client.close()
