3a87baa8814a2cf99b3a97322d12dfe740fbd68a
Goose <cybergoose13@gmail.com>
2025-09-07 17:19:56 -0700

    update tcpclient

--- tcpclientCH2.py
+++ tcpclientCH2.py
@@ -5,14 +5,18 @@ import socket
 # I had to get the IP address by using socket.gethostbyname
 # this way IP was returned and passed into client.connect
 # target_host = 'www.google.com'
-target_host = socket.gethostbyname('www.google.com')
-target_port = 80
+
+target_host = socket.gethostbyname('localhost')
+# target_host = socket.gethostbyname('www.google.com')
+target_port = 9998
+# target_port = 80
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((target_host, target_port))
 
-client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+# client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+client.send(b'Hello, Server!')
 
 response = client.recv(4096)
 
